7e4ff150d4d1ae2f5e50d786270a750e137e8477
svikhlyantsev <hbpron@gmail.com>
2015-04-05 16:26:13 -0400

    Helpers
    Split off Helper functions and created the V5 base code

---
+++ Rotor Control Code/FlightControllerV5/FlightControllerV5.ino
@@ -0,0 +1,20 @@
+// FlightControllerV5.ino
+#include <PID_v1.h>
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM303_U.h>
+#include <Adafruit_BMP085_U.h>
+#include <Adafruit_10DOF.h>
+#include <Adafruit_L3GD20_U.h>
+#include <PinChangeInt.h>
+#include <Kalman.h>
+#include <helper.h>
+void setup() {
+
+}
+
+void loop() {
+
+}
+
---
+++ Rotor Control Code/FlightControllerV5/helper.cpp
@@ -0,0 +1,26 @@
+#include "helper.h"
+#include <math.h>	
+
+double map_double(double x, double in_min, double in_max, double out_min, double out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+double toDeg(double rad){
+ return rad*180/M_PI; 
+}
+
+double toRad(double deg){
+	return deg*M_PI/180;
+}
+
+double lpfBasic(double data, double filterVal, double smoothVal){
+	if (filterVal > 1){      // check to make sure param's are within range
+	    filterVal = .999;
+	}
+	else if (filterVal <= 0){
+		filterVal = 0;
+	}
+	smoothVal = data*(1.0 - filterVal) + smoothVal*filterVal; 
+	return smoothVal; 
+}
